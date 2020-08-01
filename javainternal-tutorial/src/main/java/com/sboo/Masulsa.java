05b198c9616e9278ae1594939f71cd5eacafbb02
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-01 18:34:56 +0900

    Class Handle not by class loading but with String.

--- javainternal-tutorial/src/main/java/com/sboo/Masulsa.java
+++ javainternal-tutorial/src/main/java/com/sboo/Masulsa.java
@@ -1,7 +1,9 @@
 package com.sboo;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,14 +12,20 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Masulsa {
     public static void main(String[] args){
-//        try {
-//            new ByteBuddy().redefine(Moja.class)
-//                    .method(named("pullOut")).intercept(FixedValue.value("Rabbit"))
-//                    .make().saveIn(new File("/Users/sboo/Desktop/Development/Backend/Java/learning-java/javainternal-tutorial/target/classes/"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        ClassLoader classLoader = Masulsa.class.getClassLoader();
+        TypePool typePool = TypePool.Default.of(classLoader);
+
+        try {
+            new ByteBuddy().redefine(
+                    typePool.describe("com.sboo.Moja").resolve(),
+                    ClassFileLocator.ForClassLoader.of(classLoader))
+                    .method(named("pullOut")).intercept(FixedValue.value("Rabbit"))
+                    .make().saveIn(new File("/Users/sboo/Desktop/Development/Backend/Java/learning-java/javainternal-tutorial/target/classes/"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         System.out.println(new Moja().pullOut());
+        //여기가 최초로 Moja class 를 로드하는 포인트가 됨.
     }
 }
