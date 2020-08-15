f27eb8c77206b0d9ee188c655c7244fbbb4cfbf9
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-15 15:06:51 +0900

    Annotation Processor Basic

---
+++ javainternal-annotation-processor/src/main/java/com/sboo/MagicMojaProcessor.java
@@ -0,0 +1,39 @@
+package com.sboo;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Name;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.util.Set;
+
+public class MagicMojaProcessor extends AbstractProcessor {
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Set.of(Magic.class.getName());
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(Magic.class);
+        for(Element element: elements) {
+            Name elementName = element.getSimpleName();
+            if(element.getKind() != ElementKind.INTERFACE) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,  "Magic Annotation cannot be used on " + elementName) ;
+                //Compile Error 유도.
+            } else {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "Processing " + elementName);
+            }
+        }
+        return true;
+    }
+}
