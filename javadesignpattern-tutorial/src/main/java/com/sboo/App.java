9944e2ea633955e6bbc14c30b730bb2f947d2abf
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-16 18:18:54 +0900

    Normal use of class/instance

--- javadesignpattern-tutorial/src/main/java/com/sboo/App.java
+++ javadesignpattern-tutorial/src/main/java/com/sboo/App.java
@@ -10,32 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        Date date = new Date();
-        System.out.println(date);
+        Database d1 = new Database("1");
+        Database d2 = new Database("2");
+        Database d3 = new Database("3");
+        Database d4 = new Database("4");
+        Database d5 = new Database("5");
+        Database d6 = new Database("6");
+
+         System.out.println("database use");
     }
-
-    public static void getDate(Date d) {
-        Date date = d;
-        System.out.println(date);
-    }
-
-    public static void memberUse1() {
-        //강한 결합: 직접 생성
-        Member m1 = new Member();
-        //생성자 public -> private : 에러 발생
-    }
-
-    public static void memberUse2(Member m) {
-        //약한 결합: 생성된 것을 주입 받음. - 의존 주입(Dependency Injection)
-        //다른 클래스의 변화에 더 안전하고 유연하게 대처할 수 있음.
-        Member m2 = m;
-    }
-}
-
-//Member를 사용한다 --> Member의 기능에 의존한다.
-class Member {
-    String name;
-    String nickname;
-
-    private Member() {}
 }
---
+++ javadesignpattern-tutorial/src/main/java/com/sboo/Database.java
@@ -0,0 +1,13 @@
+package com.sboo;
+
+public class Database {
+    private String name;
+
+    public Database(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
