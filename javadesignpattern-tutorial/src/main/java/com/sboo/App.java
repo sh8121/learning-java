1b69363e2c1aef6945293c6e2bf52a5db92b83ed
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-16 18:31:40 +0900

    apply singleton

--- javadesignpattern-tutorial/src/main/java/com/sboo/App.java
+++ javadesignpattern-tutorial/src/main/java/com/sboo/App.java
@@ -10,13 +10,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        Database d1 = new Database("1");
-        Database d2 = new Database("2");
-        Database d3 = new Database("3");
-        Database d4 = new Database("4");
-        Database d5 = new Database("5");
-        Database d6 = new Database("6");
+        Database database;
+        database = Database.getInstance("첫번째 Database");
+        System.out.println("This is the " + database.getName());
 
-         System.out.println("database use");
+        database = Database.getInstance("두번째 Database");
+        System.out.println("This is the " + database.getName());
     }
 }
--- javadesignpattern-tutorial/src/main/java/com/sboo/Database.java
+++ javadesignpattern-tutorial/src/main/java/com/sboo/Database.java
@@ -1,10 +1,18 @@
 package com.sboo;
 
 public class Database {
+    private static Database singleton;
     private String name;
 
     public Database(String name) {
         this.name = name;
+    }
+
+    public static Database getInstance(String name){
+        if(singleton == null) {
+            singleton = new Database(name);
+        }
+        return singleton;
     }
 
     public String getName() {
