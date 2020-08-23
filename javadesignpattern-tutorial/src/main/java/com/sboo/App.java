8ad97495eb34fe0cb7fb079867eaea449eae8e7d
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-23 14:43:05 +0900

    Flyweight Pattern Implementation

--- javadesignpattern-tutorial/src/main/java/com/sboo/App.java
+++ javadesignpattern-tutorial/src/main/java/com/sboo/App.java
@@ -8,28 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        MyData md1 = new MyData();
-        md1.xpos = 10;
-        md1.ypos = 11;
-        md1.name = "홍길동";
-
-        MyData md2 = new MyData();
-        md2 = md1;
-        //객체 Pool에서 같은 객체 공유.
-        //얕은 복사도 Flyweight Pattern의 좋은 예.
-
-        MyData md3 = new MyData();
-        md3.xpos = 20;
-        md3.ypos = 21;
-        md3.name = "손오공";
-
-        md2.name = "전우치";
-        md2.xpos = 5;
+        FlyweightFactory flyweight = new FlyweightFactory();
+        flyweight.getSubject("a");
+        flyweight.getSubject("a");
+        flyweight.getSubject("b");
+        flyweight.getSubject("b");
     }
-}
-
-class MyData {
-    int xpos;
-    int ypos;
-    String name;
 }
---
+++ javadesignpattern-tutorial/src/main/java/com/sboo/Subject.java
@@ -0,0 +1,9 @@
+package com.sboo;
+
+public class Subject {
+    private String name;
+
+    public Subject(String name) {
+        this.name = name;
+    }
+}
