57e06595a5705c1ea31eb6a884dda57311d086d5
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-08-30 16:30:25 +0900

    Facade Pattern - Simple Facade pattern sample

--- javadesignpattern-tutorial/src/main/java/com/sboo/APlayer.java
+++
@@ -1,6 +0,0 @@
-package com.sboo;
-
-public interface APlayer {
-    void play(String fileName);
-    void stop();
-}
--- javadesignpattern-tutorial/src/main/java/com/sboo/BPlayer.java
+++
@@ -1,6 +0,0 @@
-package com.sboo;
-
-public abstract class BPlayer {
-    public abstract void playFile(String fileName);
-    public abstract void stopFile();
-}
