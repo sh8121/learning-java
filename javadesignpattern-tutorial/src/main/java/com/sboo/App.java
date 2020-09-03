6f3b9f4c3c1314c89c833f85c6f3db6b29b26609
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-09-03 21:30:30 +0900

    Bridge Pattern - 구현의 계층/기능의 계층

--- javadesignpattern-tutorial/src/main/java/com/sboo/App.java
+++ javadesignpattern-tutorial/src/main/java/com/sboo/App.java
@@ -1,22 +1,23 @@
 package com.sboo;
 
-// 기능의 추가 : 행동(요리) 추가
+// 기능의 추가 : 행동 추가
+// 추가된 기능에 대한 구현의 추가 : 요리, 청소 등 추가
 public class App 
 {
     public static void main( String[] args )
     {
-        IRobot robot = new RobotModel1();
-        Cook work = new Cook(robot);
-        work.powerOn();
-        work.doCook();
-        work.powerOff();
+        IRobot robot1 = new RobotModel1();
+        Cook work1 = new Cook(robot1);
+        work1.powerOn();
+        work1.doCook();
+        work1.powerOff();
 
         System.out.println("--------------------");
 
-        robot = new RobotModel2();
-        work = new Cook(robot);
-        work.powerOn();
-        work.doCook();
-        work.powerOff();
+        IRobot robot2 = new RobotModel1();
+        Clean work2 = new Clean(robot2);
+        work2.powerOn();
+        work2.doClean();
+        work2.powerOff();
     }
 }
