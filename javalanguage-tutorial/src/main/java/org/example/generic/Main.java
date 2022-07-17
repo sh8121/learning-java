0c72952b280c4b2023352b30c4cdfcce0cd8e608
부상훈 <sboo@gmarket.com>
2022-07-17 19:32:56 +0900

    extends/super

--- javalanguage-tutorial/src/main/java/org/example/generic/Main.java
+++ javalanguage-tutorial/src/main/java/org/example/generic/Main.java
@@ -2,10 +2,13 @@ package org.example.generic;
 
 public class Main {
     public static void main(String[] args) {
-        Tv tv = new Tv("티비");
-        Radio radio = new Radio("라디오");
+//        Tv tv = new Tv("티비");
+//        Radio radio = new Radio("라디오");
+//
+//        RemoteController<? extends Electronics> tvRemoteController = new RemoteController<Tv>(tv);
+//        RemoteController<? extends Electronics> radioRemoteController = new RemoteController<Radio>(radio);
 
-        RemoteController<?> tvRemoteController = new RemoteController<Tv>(tv);
-        RemoteController<?> radioRemoteController = new RemoteController<Radio>(radio);
+        Electronics electronics = new Electronics();
+        RemoteController<? super Tv> remoteController = new RemoteController<Electronics>(electronics);
     }
 }
