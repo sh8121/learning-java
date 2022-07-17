a6745dd0713818f499682221040ad65d8b773414
부상훈 <sboo@gmarket.com>
2022-07-17 11:58:25 +0900

    fix local variable name

--- javalanguage-tutorial/src/main/java/org/example/generic/Main.java
+++ javalanguage-tutorial/src/main/java/org/example/generic/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Tv tv1 = new Tv("티비1");
         Radio radio1 = new Radio("라디오1");
-        RemoteController<Tv> tvRemoteController1 = new RemoteController<Tv>(tv1);
-        RemoteController<Radio> radioRemoteController1 = new RemoteController<Radio>(radio1);
+        RemoteController<Tv> tvRemoteController = new RemoteController<Tv>(tv1);
+        RemoteController<Radio> radioRemoteController = new RemoteController<Radio>(radio1);
 
-        System.out.println(tvRemoteController1.getClass());
-        System.out.println(radioRemoteController1.getClass());
+        System.out.println(tvRemoteController.getClass());
+        System.out.println(radioRemoteController.getClass());
     }
 }
