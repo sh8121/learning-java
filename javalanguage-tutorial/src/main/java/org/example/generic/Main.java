45f2e9362aba692e761e45be26c4f3b0695b8088
부상훈 <sboo@gmarket.com>
2022-09-24 11:45:58 +0900

    Collection Generic Compile Time Type check

--- javalanguage-tutorial/src/main/java/org/example/generic/Main.java
+++ javalanguage-tutorial/src/main/java/org/example/generic/Main.java
@@ -5,15 +5,24 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        List<? super Electronics> list = new ArrayList<Electronics>();
-//        list.add(new Tv("티비"));
-//        list.add(new Radio("라디오"));
-//        list.add(new Electronics());
-//
-//        for(Object e : list) { //compile error
-//            System.out.println(e);
-//        }
-//
-//        List<Electronics>
+        List<Tv> tvList = new ArrayList<>();
+        tvList.add(new Tv("제조사A", "티비A"));
+        tvList.add(new Tv("제조사A", "티비B"));
+        tvList.add(new Tv("제조사B", "티비C"));
+//        tvList.add(new Radio("제조사A", "라디오A")); // Compile Error
+
+        List<Radio> radioList = new ArrayList<>();
+        radioList.add(new Radio("제조사A", "라디오A"));
+        radioList.add(new Radio("제조사B", "라디오B"));
+        radioList.add(new Radio("제조사B", "라디오C"));
+//        radioList.add(new Tv("제조사A", "티비A")); // Compile Error
+
+        for (Tv tv : tvList) {
+            System.out.println("tv.title = " + tv.getTitle());
+        }
+
+        for (Radio radio : radioList) {
+            System.out.println("radio.name = " + radio.getName());
+        }
     }
 }
