22f92b87e618e4459f32bcaaf854e70f8d184433
부상훈 <sboo@gmarket.com>
2022-09-24 12:50:05 +0900

    use-site variance

--- javalanguage-tutorial/src/main/java/org/example/generic/Main.java
+++ javalanguage-tutorial/src/main/java/org/example/generic/Main.java
@@ -6,15 +6,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List tvList = new ArrayList();
-        tvList.add(new Tv("제조사A", "티비A"));
-        tvList.add(new Tv("제조사A", "티비B"));
-        tvList.add(new Tv("제조사B", "티비C"));
-        tvList.add(new Radio("제조사A", "라디오A"));
-
-        for (Object o : tvList) {
-            Tv tv = (Tv)o; //ClassCastException
-            System.out.println("tv.title = " + tv.getTitle());
-        }
+        List<Tv> tvList;
+        tvList = new ArrayList<Tv>();
+//        tvList = new ArrayList<Radio>(); //Compile Error
     }
 }
