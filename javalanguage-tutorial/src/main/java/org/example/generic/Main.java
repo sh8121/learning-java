f44b8a77f2dac224160d759c47fa35a82ed4216d
부상훈 <sboo@gmarket.com>
2022-09-24 12:08:10 +0900

    Runtime ClassCastException

--- javalanguage-tutorial/src/main/java/org/example/generic/Main.java
+++ javalanguage-tutorial/src/main/java/org/example/generic/Main.java
@@ -1,28 +1,20 @@
 package org.example.generic;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Tv> tvList = new ArrayList<>();
+        List tvList = new ArrayList();
         tvList.add(new Tv("제조사A", "티비A"));
         tvList.add(new Tv("제조사A", "티비B"));
         tvList.add(new Tv("제조사B", "티비C"));
-//        tvList.add(new Radio("제조사A", "라디오A")); // Compile Error
+        tvList.add(new Radio("제조사A", "라디오A"));
 
-        List<Radio> radioList = new ArrayList<>();
-        radioList.add(new Radio("제조사A", "라디오A"));
-        radioList.add(new Radio("제조사B", "라디오B"));
-        radioList.add(new Radio("제조사B", "라디오C"));
-//        radioList.add(new Tv("제조사A", "티비A")); // Compile Error
-
-        for (Tv tv : tvList) {
+        for (Object o : tvList) {
+            Tv tv = (Tv)o; //ClassCastException
             System.out.println("tv.title = " + tv.getTitle());
-        }
-
-        for (Radio radio : radioList) {
-            System.out.println("radio.name = " + radio.getName());
         }
     }
 }
