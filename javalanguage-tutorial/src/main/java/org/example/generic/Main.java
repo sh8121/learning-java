1575e8f0c3877ea39553fd0d419f5258ed7e2813
부상훈 <sboo@gmarket.com>
2022-09-26 09:46:12 +0900

    Generic Use with Inheritance

--- javalanguage-tutorial/src/main/java/org/example/generic/Main.java
+++ javalanguage-tutorial/src/main/java/org/example/generic/Main.java
@@ -6,7 +6,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ElectronicsList<Electronics> electronicsList1;
-//        ElectronicsList<Object> electronicsList2; // Compile Error
+        ElectronicsList<Electronics> electronicsList = new ElectronicsList<>();
+        electronicsList.add(new Tv("제조사1", "티비1"));
+        electronicsList.add(new Tv("제조사2", "티비2"));
+        electronicsList.add(new Radio("제조사3", "라디오1"));
+        electronicsList.add(new Radio("제조사4", "라디오2"));
+
+        printManufacturer(electronicsList);
+    }
+
+    private static void printManufacturer(List<Electronics> electronicsList) {
+        for (Electronics electronics : electronicsList) {
+            System.out.println("electronics.manufacturer = " + electronics.getManufacturer());
+        }
     }
 }
